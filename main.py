c958266a2cabf7614da7e7310a8333397ba4e3e2
Nitish Kumar S <135323641+nitish07r@users.noreply.github.com>
2026-08-01 23:07:39 +0530

    Make app startup compatible with Railway

--- main.py
+++ main.py
@@ -728,3 +728,14 @@ def create_admin(
     db.refresh(new_admin)
 
     return new_admin
+
+
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8000)),
+    )
